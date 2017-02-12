63d7cf275b68368f1c45ecd1d7963be68c3e40ec
dotcomboom <38927017+dotcomboom@users.noreply.github.com>
2017-02-12 03:17:28 +0000

    Cut, copy, paste, undo, redo, saving/opening files, and inserting tags Imported from Changeset 53

--- PhotoPage Studio/PhotoPage Studio/Editor.Designer.vb
+++ PhotoPage Studio/PhotoPage Studio/Editor.Designer.vb
@@ -25,7 +25,37 @@ Partial Class Editor
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Editor))
         Me.FastColoredTextBox1 = New FastColoredTextBoxNS.FastColoredTextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.UndoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RedoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.CutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PasteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SelectAllToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.TagsToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PageTitleToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PageDescriptionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FooterTextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator4 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ContentToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator5 = New System.Windows.Forms.ToolStripSeparator()
+        Me.FontsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BackgroundColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator6 = New System.Windows.Forms.ToolStripSeparator()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
         CType(Me.FastColoredTextBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'FastColoredTextBox1
@@ -37,6 +67,7 @@ Partial Class Editor
         Me.FastColoredTextBox1.CharHeight = 14
         Me.FastColoredTextBox1.CharWidth = 8
         Me.FastColoredTextBox1.CommentPrefix = Nothing
+        Me.FastColoredTextBox1.ContextMenuStrip = Me.ContextMenuStrip1
         Me.FastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
         Me.FastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer))
         Me.FastColoredTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
@@ -44,16 +75,183 @@ Partial Class Editor
         Me.FastColoredTextBox1.Language = FastColoredTextBoxNS.Language.HTML
         Me.FastColoredTextBox1.LeftBracket = Global.Microsoft.VisualBasic.ChrW(60)
         Me.FastColoredTextBox1.LeftBracket2 = Global.Microsoft.VisualBasic.ChrW(40)
-        Me.FastColoredTextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.FastColoredTextBox1.Location = New System.Drawing.Point(0, 24)
         Me.FastColoredTextBox1.Name = "FastColoredTextBox1"
         Me.FastColoredTextBox1.Paddings = New System.Windows.Forms.Padding(0)
         Me.FastColoredTextBox1.RightBracket = Global.Microsoft.VisualBasic.ChrW(62)
         Me.FastColoredTextBox1.RightBracket2 = Global.Microsoft.VisualBasic.ChrW(41)
         Me.FastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.FastColoredTextBox1.ServiceColors = CType(resources.GetObject("FastColoredTextBox1.ServiceColors"), FastColoredTextBoxNS.ServiceColors)
-        Me.FastColoredTextBox1.Size = New System.Drawing.Size(470, 313)
+        Me.FastColoredTextBox1.Size = New System.Drawing.Size(470, 289)
         Me.FastColoredTextBox1.TabIndex = 0
         Me.FastColoredTextBox1.Zoom = 100
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UndoToolStripMenuItem, Me.RedoToolStripMenuItem, Me.ToolStripSeparator2, Me.CutToolStripMenuItem, Me.CopyToolStripMenuItem, Me.PasteToolStripMenuItem, Me.ToolStripSeparator1, Me.SelectAllToolStripMenuItem, Me.ToolStripSeparator3, Me.TagsToolStripMenuItem1})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(123, 176)
+        '
+        'UndoToolStripMenuItem
+        '
+        Me.UndoToolStripMenuItem.Name = "UndoToolStripMenuItem"
+        Me.UndoToolStripMenuItem.Size = New System.Drawing.Size(122, 22)
+        Me.UndoToolStripMenuItem.Text = "Undo"
+        '
+        'RedoToolStripMenuItem
+        '
+        Me.RedoToolStripMenuItem.Name = "RedoToolStripMenuItem"
+        Me.RedoToolStripMenuItem.Size = New System.Drawing.Size(122, 22)
+        Me.RedoToolStripMenuItem.Text = "Redo"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(119, 6)
+        '
+        'CutToolStripMenuItem
+        '
+        Me.CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        Me.CutToolStripMenuItem.Size = New System.Drawing.Size(122, 22)
+        Me.CutToolStripMenuItem.Text = "Cut"
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(122, 22)
+        Me.CopyToolStripMenuItem.Text = "Copy"
+        '
+        'PasteToolStripMenuItem
+        '
+        Me.PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        Me.PasteToolStripMenuItem.Size = New System.Drawing.Size(122, 22)
+        Me.PasteToolStripMenuItem.Text = "Paste"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(119, 6)
+        '
+        'SelectAllToolStripMenuItem
+        '
+        Me.SelectAllToolStripMenuItem.Name = "SelectAllToolStripMenuItem"
+        Me.SelectAllToolStripMenuItem.Size = New System.Drawing.Size(122, 22)
+        Me.SelectAllToolStripMenuItem.Text = "Select All"
+        '
+        'ToolStripSeparator3
+        '
+        Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
+        Me.ToolStripSeparator3.Size = New System.Drawing.Size(119, 6)
+        '
+        'TagsToolStripMenuItem1
+        '
+        Me.TagsToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PageTitleToolStripMenuItem, Me.PageDescriptionToolStripMenuItem, Me.FooterTextToolStripMenuItem, Me.ToolStripSeparator4, Me.ContentToolStripMenuItem, Me.ToolStripSeparator5, Me.FontsToolStripMenuItem, Me.BackgroundColorToolStripMenuItem, Me.FontColorToolStripMenuItem})
+        Me.TagsToolStripMenuItem1.Name = "TagsToolStripMenuItem1"
+        Me.TagsToolStripMenuItem1.Size = New System.Drawing.Size(122, 22)
+        Me.TagsToolStripMenuItem1.Text = "Tags"
+        '
+        'PageTitleToolStripMenuItem
+        '
+        Me.PageTitleToolStripMenuItem.Name = "PageTitleToolStripMenuItem"
+        Me.PageTitleToolStripMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.PageTitleToolStripMenuItem.Text = "Page Title"
+        '
+        'PageDescriptionToolStripMenuItem
+        '
+        Me.PageDescriptionToolStripMenuItem.Name = "PageDescriptionToolStripMenuItem"
+        Me.PageDescriptionToolStripMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.PageDescriptionToolStripMenuItem.Text = "Page Description"
+        '
+        'FooterTextToolStripMenuItem
+        '
+        Me.FooterTextToolStripMenuItem.Name = "FooterTextToolStripMenuItem"
+        Me.FooterTextToolStripMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.FooterTextToolStripMenuItem.Text = "Footer Text"
+        '
+        'ToolStripSeparator4
+        '
+        Me.ToolStripSeparator4.Name = "ToolStripSeparator4"
+        Me.ToolStripSeparator4.Size = New System.Drawing.Size(167, 6)
+        '
+        'ContentToolStripMenuItem
+        '
+        Me.ContentToolStripMenuItem.Name = "ContentToolStripMenuItem"
+        Me.ContentToolStripMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.ContentToolStripMenuItem.Text = "Content/Photos"
+        '
+        'ToolStripSeparator5
+        '
+        Me.ToolStripSeparator5.Name = "ToolStripSeparator5"
+        Me.ToolStripSeparator5.Size = New System.Drawing.Size(167, 6)
+        '
+        'FontsToolStripMenuItem
+        '
+        Me.FontsToolStripMenuItem.Name = "FontsToolStripMenuItem"
+        Me.FontsToolStripMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.FontsToolStripMenuItem.Text = "Fonts"
+        '
+        'BackgroundColorToolStripMenuItem
+        '
+        Me.BackgroundColorToolStripMenuItem.Name = "BackgroundColorToolStripMenuItem"
+        Me.BackgroundColorToolStripMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.BackgroundColorToolStripMenuItem.Text = "Background Color"
+        '
+        'FontColorToolStripMenuItem
+        '
+        Me.FontColorToolStripMenuItem.Name = "FontColorToolStripMenuItem"
+        Me.FontColorToolStripMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.FontColorToolStripMenuItem.Text = "Font Color"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(470, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.ToolStripSeparator6, Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(156, 22)
+        Me.NewToolStripMenuItem.Text = "New Template"
+        '
+        'ToolStripSeparator6
+        '
+        Me.ToolStripSeparator6.Name = "ToolStripSeparator6"
+        Me.ToolStripSeparator6.Size = New System.Drawing.Size(153, 6)
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(156, 22)
+        Me.OpenToolStripMenuItem.Text = "Open Template"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(156, 22)
+        Me.SaveToolStripMenuItem.Text = "Save Template"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.DefaultExt = "ppgt"
+        Me.OpenFileDialog1.Filter = "PhotoPage Template (*.ppgt)|*.ppgt|HTML Document (*.html)|*.html"
+        Me.OpenFileDialog1.Title = "Open Template"
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.DefaultExt = "ppgt"
+        Me.SaveFileDialog1.Filter = "PhotoPage Template (*.ppgt)|*.ppgt|HTML Document (*.html)|*.html"
+        Me.SaveFileDialog1.Title = "Save Template"
         '
         'Editor
         '
@@ -61,14 +259,48 @@ Partial Class Editor
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(470, 313)
         Me.Controls.Add(Me.FastColoredTextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Editor"
         Me.ShowIcon = False
         Me.Text = "Template Editor"
         CType(Me.FastColoredTextBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents FastColoredTextBox1 As FastColoredTextBoxNS.FastColoredTextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents UndoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents SelectAllToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RedoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator3 As ToolStripSeparator
+    Friend WithEvents TagsToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents PageTitleToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PageDescriptionToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FooterTextToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator4 As ToolStripSeparator
+    Friend WithEvents ContentToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator5 As ToolStripSeparator
+    Friend WithEvents FontsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BackgroundColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FontColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator6 As ToolStripSeparator
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
 End Class
--- PhotoPage Studio/PhotoPage Studio/Editor.vb
+++ PhotoPage Studio/PhotoPage Studio/Editor.vb
@@ -2,4 +2,68 @@
     Private Sub Editor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub UndoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UndoToolStripMenuItem.Click
+        FastColoredTextBox1.Undo()
+    End Sub
+
+    Private Sub CutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem.Click
+        FastColoredTextBox1.Cut()
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        FastColoredTextBox1.Copy()
+    End Sub
+
+    Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
+        FastColoredTextBox1.Paste()
+    End Sub
+
+    Private Sub SelectAllToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SelectAllToolStripMenuItem.Click
+        FastColoredTextBox1.SelectAll()
+    End Sub
+
+    Private Sub RedoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RedoToolStripMenuItem.Click
+        FastColoredTextBox1.Redo()
+    End Sub
+
+    Private Sub PageTitleToolStripMenuItem_Click_1(sender As Object, e As EventArgs) Handles PageTitleToolStripMenuItem.Click
+        FastColoredTextBox1.InsertText("[#pagetitle#]")
+    End Sub
+
+    Private Sub PageDescriptionToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PageDescriptionToolStripMenuItem.Click
+        FastColoredTextBox1.InsertText("[#description#]")
+    End Sub
+
+    Private Sub FooterTextToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FooterTextToolStripMenuItem.Click
+        FastColoredTextBox1.InsertText("[#footer#]")
+    End Sub
+
+    Private Sub ContentToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ContentToolStripMenuItem.Click
+        FastColoredTextBox1.InsertText("[#content#]")
+    End Sub
+
+    Private Sub FontsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontsToolStripMenuItem.Click
+        FastColoredTextBox1.InsertText("[#fonts#]")
+    End Sub
+
+    Private Sub BackgroundColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BackgroundColorToolStripMenuItem.Click
+        FastColoredTextBox1.InsertText("[#bgcolor#]")
+    End Sub
+
+    Private Sub FontColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontColorToolStripMenuItem.Click
+        FastColoredTextBox1.InsertText("[#color#]")
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        FastColoredTextBox1.Text = ""
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        FastColoredTextBox1.OpenFile("test")
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+
+    End Sub
 End Class
