db130188674abbf22e13f356ac8b4c4c83c39cc9
dotcomboom <38927017+dotcomboom@users.noreply.github.com>
2017-02-12 03:25:21 +0000

    Hovering on main menu buttons Imported from Changeset 54

--- PhotoPage Studio/PhotoPage Studio/Welcome.Designer.vb
+++ PhotoPage Studio/PhotoPage Studio/Welcome.Designer.vb
@@ -27,6 +27,8 @@ Partial Class Welcome
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -73,11 +75,33 @@ Partial Class Welcome
         Me.PictureBox4.TabIndex = 4
         Me.PictureBox4.TabStop = False
         '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(242, 239)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(146, 21)
+        Me.Label3.TabIndex = 14
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(242, 267)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(146, 62)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
         'Welcome
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(400, 338)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.PictureBox4)
         Me.Controls.Add(Me.PictureBox3)
         Me.Controls.Add(Me.PictureBox2)
@@ -102,4 +126,6 @@ Partial Class Welcome
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents PictureBox3 As PictureBox
     Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- PhotoPage Studio/PhotoPage Studio/Welcome.vb
+++ PhotoPage Studio/PhotoPage Studio/Welcome.vb
@@ -4,4 +4,24 @@
         editor.MdiParent = Form1
         editor.Show()
     End Sub
+
+    Private Sub PictureBox2_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox2.MouseEnter
+        Label3.Text = "New Template"
+        Label2.Text = "Open a new Template Editor window."
+    End Sub
+
+    Private Sub PictureBox3_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox3.MouseEnter
+        Label3.Text = "Open Template"
+        Label2.Text = "Modify an existing template."
+    End Sub
+
+    Private Sub PictureBox4_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox4.MouseEnter
+        Label3.Text = "Tutorial"
+        Label2.Text = "Learn how to create your own templates."
+    End Sub
+
+    Private Sub Welcome_MouseEnter(sender As Object, e As EventArgs) Handles MyBase.MouseEnter
+        Label3.Text = ""
+        Label2.Text = ""
+    End Sub
 End Class
