3b6548c452eff13cc43753d3c79596b23682f4e8
dotcomboom <38927017+dotcomboom@users.noreply.github.com>
2017-02-14 02:29:30 +0000

    Update fixed icon Imported from Changeset 75

--- PhotoPage Studio/PhotoPage Studio/Welcome.Designer.vb
+++ PhotoPage Studio/PhotoPage Studio/Welcome.Designer.vb
@@ -129,7 +129,7 @@ Partial Class Welcome
         '
         Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
         Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(162, 17)
-        Me.ToolStripStatusLabel2.Text = "PhotoPage Studio 20170212.1"
+        Me.ToolStripStatusLabel2.Text = "PhotoPage Studio 20170212.2"
         '
         'TextBox1
         '
